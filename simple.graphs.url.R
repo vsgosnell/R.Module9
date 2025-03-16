13b94cb0da1cd6a810c52e6d2f4dcfcc9fcfab7b
Veronica Gosnell <vsgosnell@usf.edu>
2025-03-16 15:11:58 -0400

    initial commit

---
+++ simple.graphs.url.R
@@ -0,0 +1,92 @@
+#producing simple graphs with R
+
+# line charts
+
+# Define the cars vector with 5 values
+cars <- c(1, 3, 6, 4, 9)
+
+# Graph the cars vector with all defaults
+plot(cars)
+
+
+
+
+# Let's add a title, a line to connect the points, and some color:
+
+# Define the cars vector with 5 values
+cars <- c(1, 3, 6, 4, 9)
+
+# Graph cars using blue points overlayed by a line 
+plot(cars, type="o", col="blue")
+
+# Create a title with a red, bold/italic font
+title(main="Autos", col.main="red", font.main=4)
+
+
+
+# Now let's add a red line for trucks and specify the y-axis range 
+# directly so it will be large enough to fit the truck data:
+
+# Define 2 vectors
+cars <- c(1, 3, 6, 4, 9)
+trucks <- c(2, 5, 4, 5, 12)
+
+# Graph cars using a y axis that ranges from 0 to 12
+plot(cars, type="o", col="blue", ylim=c(0,12))
+
+# Graph trucks with red dashed line and square points
+lines(trucks, type="o", pch=22, lty=2, col="red")
+
+# Create a title with a red, bold/italic font
+title(main="Autos", col.main="red", font.main=4)
+
+
+
+# Next, let's change the axes labels to match our data and add a legend. 
+# We'll also compute the y-axis values using the max function 
+# so any changes to our data will be automatically reflected in our graph.
+
+# Define 2 vectors
+cars <- c(1, 3, 6, 4, 9)
+trucks <- c(2, 5, 4, 5, 12)
+
+# Calculate range from 0 to max value of cars and trucks
+g_range <- range(0, cars, trucks)
+
+# Graph autos using y axis that ranges from 0 to max 
+# value in cars or trucks vector.  Turn off axes and 
+# annotations (axis labels) so we can specify them ourself
+plot(cars, type="o", col="blue", ylim=g_range, 
+     axes=FALSE, ann=FALSE)
+
+# Make x axis using Mon-Fri labels
+axis(1, at=1:5, lab=c("Mon","Tue","Wed","Thu","Fri"))
+
+# Make y axis with horizontal labels that display ticks at 
+# every 4 marks. 4*0:g_range[2] is equivalent to c(0,4,8,12).
+axis(2, las=1, at=4*0:g_range[2])
+
+# Create box around plot
+box()
+
+# Graph trucks with red dashed line and square points
+lines(trucks, type="o", pch=22, lty=2, col="red")
+
+# Create a title with a red, bold/italic font
+title(main="Autos", col.main="red", font.main=4)
+
+# Label the x and y axes with dark green text
+title(xlab="Days", col.lab=rgb(0,0.5,0))
+title(ylab="Total", col.lab=rgb(0,0.5,0))
+
+# Create a legend at (1, g_range[2]) that is slightly smaller 
+# (cex) and uses the same line colors and points used by 
+# the actual plots 
+legend(1, g_range[2], c("cars","trucks"), cex=0.8, 
+       col=c("blue","red"), pch=21:22, lty=1:2);
+
+
+
+
+
+
